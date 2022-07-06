fca88e3b3a632e5b8db05f4e73a023e4b8b6c260
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2022-07-06 15:13:33 -0500

    Setup basic config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { FC } from 'react';
+
+const App: FC = (): JSX.Element => {
+  return (
+    <div className="App">
+      <h1>Magnify: Better than Course Inspector.</h1>
+    </div>
+  );
+};
+
+export default App;
